5260cc0af5016cce440689f2cc4eb2d081b25459
lvquan2003 <levanquan051003@gmail.com>
2024-12-12 13:23:50 +0700

    fix somebug for deploy

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,12 +5,11 @@ import ApiError from '~/utils/ApiError'
 
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    // Update mới: Ở video số 75 trong chuỗi MERN Stack PRO khi chúng ta deploy dự án lên một Server Production thì sẽ sửa lại đoạn này thêm một chút nữa để phù hợp với từng môi trường production hoặc dev nhé. Học với mình thì các bạn cứ yên tâm về sự chỉn chu chuẩn chỉnh nhé :D
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Neu moi truong dev thi cho qua luon
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
+    //Nguoc lai, la moi truong production
 
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
